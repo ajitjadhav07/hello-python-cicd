71774c142a6f38cdccf7ffcc9dcdec40e5763301
ajitjadhav07 <jadhavajit910.sits.it@gmail.com>
2026-02-17 18:24:10 +0530

    new changes is done for Testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Vicky from my CI/CD pipeline"
+    return "Hello RAJIV sir from my CI/CD pipeline"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
