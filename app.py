ac4dad3fcb6949dbf5b9a9a9d1535652a5fb8311
Ajit Jadhav <105701428+ajitjadhav07@users.noreply.github.com>
2026-02-27 16:50:47 +0530

    Update hello function response message

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, Testing  from my CI/CD Pipeline"
+    return "Hello World, Testing  from my CI/CD Pipeline abcd"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
